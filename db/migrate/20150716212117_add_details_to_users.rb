a880bba9c53497a64373360272f73ba21a8569bd
Jose Rosado <joserosado@esd.com.do>
2015-07-16 18:28:51 -0400

    Added username, name, last name and role to user model.

---
+++ db/migrate/20150716212117_add_details_to_users.rb
@@ -0,0 +1,9 @@
+class AddDetailsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+    add_index  :users, :username, unique: true
+    add_column :users, :name, :string
+    add_column :users, :lastname, :string
+    add_column :users, :role, :string
+  end
+end
