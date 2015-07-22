5094af375bc2e15dc65e017ef2be972e85bef89c
Jose Rosado <joserosado@esd.com.do>
2015-07-22 10:12:22 -0400

    Scope by user role

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -3,7 +3,15 @@ class Wiki < ActiveRecord::Base
   validates :user, presence:true
   after_initialize :init
 
-  scope :visible_to, -> (user) { where(["private = ? or user_id =?", false, user.id ]) }
+  scope :visible_to, -> (user) {
+    if user.admin?
+      all
+    elsif user.premium?
+      where(["private = ? or user_id =?", false, user.id ])
+    else
+      where(["private = ?", false ])
+    end
+  }
 
   def init
     self.private == false if (self.has_attribute? :private) && self.private.nil?
